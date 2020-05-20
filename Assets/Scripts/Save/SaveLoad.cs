4d5dcd0d27667068bb394d1a14bbd6f8daef92a5
anguy129 <anguy129@ucr.edu>
2020-05-20 12:55:14 -0700

    Save Load Menu working
    currently only saving position

--- Assets/Scripts/Save/SaveLoad.cs
+++ Assets/Scripts/Save/SaveLoad.cs
@@ -1,27 +1,74 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
- 
-public static class SaveLoad {
- 
-    public static List<Game> savedGames = new List<Game>();
+using Lowscope;
+using UnityEngine.SceneManagement;
 
-    public static void Save() {
-        savedGames.Add(Game.current);
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create (Application.persistentDataPath + "/savedGames.gd");
-        bf.Serialize(file, SaveLoad.savedGames);
-        file.Close();
-    }
+ 
+namespace Lowscope.Saving.Core
+{
+    public class SaveLoad: MonoBehaviour
+    {
+        public PlayerMovement movscript;
+        public int[] saves;
+        public bool active = false;
 
-    public static void Load() {
-    if(File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-        SaveLoad.savedGames = (List<Game>)bf.Deserialize(file);
-        file.Close();
+        public void SaveSlot(int slot) {
+            Debug.Log("Saving");
+            SaveMaster.SetSlot(slot, false);
+            SaveMaster.WriteActiveSaveToDisk();     //write save to disk
+
+            Text txt = GetComponentInChildren<Text>(true);
+            txt.text = "Saved Slot";
+        }
+
+        public void LoadGame(int slot) {
+            //var slotToLoad = 0; // Set your index here
+            //SaveMaster.ClearSlot();
+            SceneManager.LoadScene("SampleScene");
+            SaveMaster.SetSlot(slot, true);
+        }
+
+        void Start()
+        {
+
+            Debug.Log("Getting Used Save Slots");
+            saves = SaveFileUtility.GetUsedSlots();
+            Debug.Log("Saves: " + saves.Length.ToString());
+            Debug.Log("Active Slot: " + SaveMaster.GetActiveSlot().ToString());
+            
+            Button[] button = GameObject.Find("SaveContent").GetComponentsInChildren<Button>();
+            int i = 0;
+            foreach(Button btn in button)
+            {
+                Text txt = btn.GetComponentInChildren<Text>(true);
+                //SaveFileUtility.LoadSave(i);
+            
+                if(SaveFileUtility.IsSlotUsed(i))
+                {
+                    txt.text = "Saved Slot";
+                }
+                else if(!SaveFileUtility.IsSlotUsed(i)){
+                    txt.text = "New Slot";
+                }
+                i++;
+            }
+        }
+        void Update()
+        {
+            
+            // DisplaySlot(1);
+            // DisplaySlot(2);
+            // DisplaySlot(3);
+            // DisplaySlot(4);
+            // DisplaySlot(5);
+            // DisplaySlot(6);
+            // DisplaySlot(7);
+            // DisplaySlot(8);
+            // DisplaySlot(9);
+        }
     }
-}
 }
